f70cb733b5048a2fc8e42caa6697d39a396a7d22
Andrew Turner <turner.andrew0@gmail.com>
2021-11-03 11:35:33 -0500

    js

--- server/index.js
+++ server/index.js
@@ -3,12 +3,15 @@ const path = require('path')
 
 const app = express()
 
+app.use('/js', express.static(path.join(__dirname, 'public/main.js')))
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
 })
 
-app.use('/js', express.static(path.join(__dirname, 'public/main.js')))
+app.get('/js', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/main.js'))
+  })
 
 const port = process.env.PORT || 4005
 
